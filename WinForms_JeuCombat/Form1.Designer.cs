0986e35c8d02c8924d2e0f454046a36ee707c6e1
Utilisateur <Utilisateur@DESKTOP-00K0V9D>
2024-10-18 09:56:41 +0200

    Ajoutez des fichiers projet.

---
+++ WinForms_JeuCombat/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WinForms_JeuCombat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            textBox1 = new TextBox();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Transparent;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(597, 110);
+            button1.TabIndex = 0;
+            button1.TextImageRelation = TextImageRelation.ImageAboveText;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.ForeColor = Color.Transparent;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(12, 128);
+            button2.Name = "button2";
+            button2.Size = new Size(597, 106);
+            button2.TabIndex = 1;
+            button2.TextImageRelation = TextImageRelation.ImageAboveText;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 75F);
+            label1.Location = new Point(12, 237);
+            label1.Name = "label1";
+            label1.Size = new Size(321, 133);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(0, -30);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Tag = "1";
+            button3.Text = "Damager";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(0, -30);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 4;
+            button4.Tag = "2";
+            button4.Text = "Healer";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button3_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(0, -30);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 5;
+            button5.Tag = "3";
+            button5.Text = "Tank";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button3_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(0, -30);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 6;
+            button6.Tag = "4";
+            button6.Text = "Assassin";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.AcceptsReturn = true;
+            textBox1.Location = new Point(637, 57);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(583, 265);
+            textBox1.TabIndex = 7;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(625, 328);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 8;
+            button7.Tag = "1";
+            button7.Text = "Attack";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(625, 357);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 9;
+            button8.Tag = "2";
+            button8.Text = "Defend";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button7_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(625, 386);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 23);
+            button9.TabIndex = 10;
+            button9.Tag = "3";
+            button9.Text = "Spell";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button7_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(textBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox1;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
---
+++ WinForms_JeuCombat/Form1.cs
@@ -0,0 +1,450 @@
+using System.Diagnostics;
+using System.Media;
+using System.Windows.Forms;
+
+namespace WinForms_JeuCombat
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttonList = new List<Button>();
+
+
+        private bool canChange = true;
+        private bool choseCharacter = false;
+        public bool choseAction = false;
+
+        private Button choiceButton;
+
+        private SoundPlayer sPlayer;
+        private SoundPlayer mSoundPlayer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get half of screen height and width + half of button height and width to find the perfect center of the screen
+            button1.Location = new Point((this.Width / 2) - (button1.Width / 2), (this.Height / 2) - (button1.Height / 2));
+            button2.Location = new Point((this.Width / 2) - (button2.Width / 2), (this.Height / 2 + 150) - (button2.Height / 2));
+
+            label1.Location = new Point((this.Width / 2) - (label1.Width / 2), 150);
+
+
+            //Add character choice buttons to list
+            buttonList.AddRange(new Button[] { button3, button4, button5, button6 });
+
+        }
+
+        public async void BounceFunction(Control control, Point targetPos, Point targetPos2, int speed)
+        {
+
+            while (control.Location.Y > targetPos.Y)//< to end position
+            {
+                speed += 1;//Incremental speed
+                control.Location = new Point(control.Location.X, control.Location.Y - speed);//Change position with speed
+                await Task.Delay(20);//Wait 20ms without freezing program
+            }
+
+            speed = -10; //Inverse speed to create bounce effect
+            while (control.Location.Y < targetPos2.Y)
+            {
+                speed += 1;
+                control.Location = new Point(control.Location.X, control.Location.Y - speed);
+                await Task.Delay(20);
+            }
+            speed = 10;
+
+            //When finished remove menu
+            control.Location = targetPos;
+            this.Controls.Remove(control);
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //ANimate controls leaving screen
+            BounceFunction(button1, new Point(0, 250), new Point(0, 500), 1);
+            BounceFunction(button2, new Point(0, 500), new Point(0, 550), 1);
+            BounceFunction(label1, new Point(0, 100), new Point(0, 400), 1);
+
+            int buttonOffset = 100;//Offset
+
+            await Task.Delay(2000);//Wait 2 seconds
+
+            //Set all character choice buttons position
+            buttonList.Reverse();
+            foreach (Button button in buttonList)
+            {
+                button.Location = new Point((this.Width / 2 + 700) - (button1.Width / 2), (this.Height / 2 - buttonOffset) - (button1.Height / 2));
+                buttonOffset += 100;
+            }
+
+            //mSoundPlayer.Play();
+
+            await Task.Delay(1000);
+
+            //sPlayer.PlayLooping();
+
+            textBox1.Text += "Choisissez un personnage:\r\n1 - Damager\r\n2 - Healer\r\n3 - Tank\r\n4 - Assasin\r\n";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();//Exit the app
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Button clickedButton = sender as Button;
+
+            MainFunction(textBox1, clickedButton);
+        }
+
+        private async void button7_Click(object sender, EventArgs e)
+        {
+            Button cButton = sender as Button;
+
+            Debug.WriteLine($"Clicked button Tag: {cButton.Tag}");
+
+            choiceButton = cButton;
+
+            choseAction = true;
+        }
+
+
+
+        //Big chunk of code ahead : 
+        public enum ActionChoice
+        {
+            Attack = 1,
+            Defend = 2,
+            Spell = 3,
+        }
+
+        //Dictionnaire contenant données des classes de personnage -> chaque valeur est elle-même un dictionnaire représentant le personnage
+        static Dictionary<string, Dictionary<string, object>> character = new Dictionary<string, Dictionary<string, object>>()
+        {
+            {
+                "Damager", new Dictionary<string, object>()
+                {
+                    { "Name", "Damager" },
+                    { "HP", 3 },
+                    { "Force", 2 },
+                    { "Action", ActionChoice.Defend},
+                    { "Poisoned", false}
+                }
+            },
+            {
+                "Healer", new Dictionary<string, object>()
+                {
+                    { "Name", "Healer" },
+                    { "HP", 4 },
+                    { "Force", 1 },
+                    { "Action", ActionChoice.Defend },
+                    { "Poisoned", false}
+                }
+            },
+            {
+                "Tank", new Dictionary<string, object>()
+                {
+                    { "Name", "Tank" },
+                    { "HP", 5 },
+                    { "Force", 1 },
+                    { "Action", ActionChoice.Defend },
+                    { "Poisoned", false}
+                }
+            },
+            {
+                "Assassin", new Dictionary<string, object>()
+                {
+                    { "Name", "Assassin" },
+                    { "HP", 3 },
+                    { "Force", 1 },
+                    { "Action", ActionChoice.Defend },
+                    { "Poisoned", false}
+                }
+            },
+        };
+
+        public void MainFunction(TextBox tBox, Button button)
+        {
+            //--------- INITIALISATION -----------
+            bool isEnd = false;
+
+            Random rnd = new Random();
+
+            // Choix personnage joueur
+            Dictionary<string, object> playerCharacter = new Dictionary<string, object>(character[PlayerChooseCharacter(tBox, button)]);
+
+            // Choix personnage AI
+            Dictionary<string, object> AICharacter = new Dictionary<string, object>(character[AIChooseCharacter()]);
+
+            //Affichage choix personnages
+            tBox.Text += ($"\r\nJoueur : {playerCharacter["Name"]}\r\nIA : {AICharacter["Name"]}");
+
+            //------------- BOUCLE DU JEU  ----------------
+
+            //On affiche l'état du jeu(faire fonction)
+            DisplayHealth(playerCharacter, AICharacter, tBox);
+
+            while (!isEnd)
+            {
+                Debug.WriteLine(choseAction);
+                while (!choseAction)
+                {
+                    Application.DoEvents();//Does not freeze the app waiting for the user input
+                }
+
+                choseAction = false;
+
+                //Choix adction joueur
+                PlayerChooseAction(playerCharacter, tBox, choiceButton);
+
+                //Choix action IA
+                AIChooseAction(AICharacter);
+
+                //Combat (round)
+                Fight(playerCharacter, AICharacter, tBox);
+
+                //On affiche l'état du jeu
+                DisplayHealth(playerCharacter, AICharacter, tBox);
+                //Conditions de fin
+                isEnd = isEndGame(playerCharacter, AICharacter, tBox);
+
+            }
+        }
+
+        //---------------------------------------------
+
+
+        //Fonction combat appelé à chaque tour
+        static void Fight(Dictionary<string, object> player, Dictionary<string, object> ai, TextBox tBox)
+        {
+            //Si cible empoisonnée au tour préc = - 1 HP et on retire empoisonnement
+            if (isPoisoned(player))
+            {
+                tBox.Text += ("\r\nPoison : - 1 HP"); //pour test
+                TakeDamage(player, 1);
+                Poisoned(player, false);
+            }
+            else if (isPoisoned(ai))
+            {
+                tBox.Text += ("\r\nPoison : - 1 HP"); //pour test
+                TakeDamage(ai, 1);
+                Poisoned(ai, false);
+            }
+
+            //Display
+            ShowPlayerAction((ActionChoice)player["Action"], tBox);
+            ShowAIAction((ActionChoice)ai["Action"], tBox);
+
+            //Play
+            PlayAction(player, ai, true, tBox);
+            PlayAction(ai, player, false, tBox);
+
+        }
+
+        //Fonction jouant l'action
+        static void PlayAction(Dictionary<string, object> actionPlayer, Dictionary<string, object> otherPlayer, bool isPlayer, TextBox tBox)
+        {
+            //GET ACTIONS
+            ActionChoice actionPlayerChoice = (ActionChoice)actionPlayer["Action"];
+            ActionChoice otherPlayerChoice = (ActionChoice)otherPlayer["Action"];
+
+            if (actionPlayerChoice == ActionChoice.Spell) //SPELL
+            {
+                if ((string)actionPlayer["Name"] == "Healer") //HEAL
+                {
+                    Heal(actionPlayer, tBox);
+                }
+                else if ((string)actionPlayer["Name"] == "Assassin") //Dagues empoisonnées
+                {
+                    //Empoisonne : - 1 HP au prochain tour
+                    Poisoned(otherPlayer, true);
+
+                    tBox.Text += ("\r\nAttaque shuriken : - 1 HP, État empoisonnée"); //pour test
+
+                    //Si cible ne défend pas : - 1ptn de dégat sur le moment
+                    if (otherPlayerChoice != ActionChoice.Defend)
+                    {
+                        TakeDamage(otherPlayer, 1);
+                    }
+                }
+                else if ((string)actionPlayer["Name"] == "Tank") //POWERFULL ATTACK
+                {
+                    //Spell effect (powerfull att)
+                    int boostedDamage = (int)actionPlayer["Force"] + 1;
+                    actionPlayer["HP"] = (int)actionPlayer["HP"] - 1;
+
+                    //OTHER RAGE
+                    if (((string)otherPlayer["Name"] == "Damager") && (otherPlayerChoice == ActionChoice.Spell))
+                    {
+                        otherPlayer["HP"] = (int)otherPlayer["HP"] - boostedDamage;
+                        actionPlayer["HP"] = (int)actionPlayer["HP"] - boostedDamage;
+
+                    }
+                    else if (otherPlayerChoice == ActionChoice.Defend) //OTHER DEFEND
+                    {
+                        otherPlayer["HP"] = (int)otherPlayer["HP"] - (boostedDamage - 1);
+                    }
+                    else  //REST (Attack, Heal, powerfull attack...)
+                    {
+                        otherPlayer["HP"] = (int)otherPlayer["HP"] - boostedDamage;
+                    }
+
+                }
+            }
+            else if (actionPlayerChoice == ActionChoice.Attack) //ATTACK
+            {
+                //Cas où l'autre fait rage (damager)
+                if (((string)otherPlayer["Name"] == "Damager") && (otherPlayerChoice == ActionChoice.Spell))
+                {
+                    otherPlayer["HP"] = (int)otherPlayer["HP"] - ((int)actionPlayer["Force"]);
+                    actionPlayer["HP"] = (int)actionPlayer["HP"] - ((int)actionPlayer["Force"]);
+
+                }
+                else if (otherPlayerChoice == ActionChoice.Defend) //Cas où l'autre défend
+                {
+                    return;
+                }
+                else //le reste (attaque, heal, powerfull attack etc...)
+                {
+                    otherPlayer["HP"] = (int)otherPlayer["HP"] - ((int)actionPlayer["Force"]);
+                }
+            }
+        }
+
+
+        //Choix action joueur
+        static void PlayerChooseAction(Dictionary<string, object> player, TextBox tBox, Button button)
+        {
+            int action_player_choice = 0;
+
+            tBox.Text += ("\r\nChoisissez une action:\r\n1 - Attack\r\n2 - Defend\r\n3 - Spell");
+
+            action_player_choice = int.Parse(button.Tag.ToString());
+
+            Debug.WriteLine($"Clicked button Tag: {button.Tag}");
+            Debug.WriteLine(action_player_choice);
+
+            player["Action"] = (ActionChoice)action_player_choice;
+        }
+
+        //Choix d'action IA
+        static void AIChooseAction(Dictionary<string, object> ai)
+        {
+            Random rand = new Random();
+            int choiceNb = Enum.GetValues(typeof(ActionChoice)).Length;
+
+            ai["Action"] = (ActionChoice)rand.Next(1, choiceNb + 1);
+        }
+
+        public string PlayerChooseCharacter(TextBox tBox, Button pChoiceButton)
+        {
+            List<string> classList = new List<string>() { "Damager", "Healer", "Tank", "Assassin" };
+            int character_player_choice = 0;
+            while (character_player_choice < 1 || character_player_choice > classList.Count)
+            {
+                //tBox.Text += ("Choisissez un personnage:\r\n1 - Damager\r\n2 - Healer\r\n3 - Tank\r\n4 - Assassin\r\n");
+
+                if (!choseCharacter)
+                {
+                    character_player_choice = int.Parse(pChoiceButton.Tag.ToString());
+                    choseCharacter = true;
+
+                    this.Controls.Remove(button3);
+                    this.Controls.Remove(button4);
+                    this.Controls.Remove(button5);
+                    this.Controls.Remove(button6);
+                }
+            }
+
+            return classList[character_player_choice - 1];
+        }
+
+        //Choix d'action IA
+        static string AIChooseCharacter()
+        {
+            List<string> classList = new List<string>() { "Damager", "Healer", "Tank", "Assassin" };
+            Random rand = new Random();
+            int rand_index = rand.Next(0, classList.Count);
+            return classList[rand_index];
+        }
+
+
+        //Détecte fin de jeu
+        static bool isEndGame(Dictionary<string, object> playerCharacter, Dictionary<string, object> aiCharacter, TextBox tBox)
+        {
+            //Conditions de fin
+            bool playerIsDead = (int)playerCharacter["HP"] <= 0;
+            bool AIisDead = (int)aiCharacter["HP"] <= 0;
+
+            if (playerIsDead && AIisDead)
+            {
+                tBox.Text += ("\r\nEgalité !");
+                return true;
+            }
+            else if (AIisDead)
+            {
+                tBox.Text += ("\r\nLe joueur a gagné !");
+                return true;
+            }
+            else if (playerIsDead)
+            {
+                tBox.Text += ("\r\nl'AI a gagné !");
+                return true;
+            }
+            else return false;
+        }
+
+        //----- Fonction spell
+        static void Heal(Dictionary<string, object> charact, TextBox tBox)
+        {
+            int _health = (int)charact["HP"] + 2;
+            //Vérifie qu'on ne dépasse pas la santé max
+            charact["HP"] = Math.Min(_health, (int)character[(string)charact["Name"]]["HP"]);
+        }
+
+        //---- Fonction affichage
+        static void ShowPlayerAction(ActionChoice action, TextBox tBox)
+        {
+            tBox.Text += ($"\r\nPlayer choice : {action.ToString()}");
+        }
+
+        static void ShowAIAction(ActionChoice action, TextBox tBox)
+        {
+            tBox.Text += ($"\r\nAI choice : {action.ToString()}");
+        }
+
+        static void DisplayHealth(Dictionary<string, object> player, Dictionary<string, object> ai, TextBox tBox)
+        {
+            tBox.Text += $"\r\nHP joueur : {player["HP"]}/{character[(string)player["Name"]]["HP"]}";
+            tBox.Text += $"\r\nHP IA : {ai["HP"]}/{character[(string)ai["Name"]]["HP"]}";
+        }
+
+        static void TakeDamage(Dictionary<string, object> character, int damage)
+        {
+            //Retrait HP
+            character["HP"] = (int)character["HP"] - 1;
+        }
+
+        static void Poisoned(Dictionary<string, object> character, bool b)
+        {
+            character["Poisoned"] = b;
+        }
+
+        static bool isPoisoned(Dictionary<string, object> character)
+        {
+            return (bool)character["Poisoned"];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+        }
+    }
+}
